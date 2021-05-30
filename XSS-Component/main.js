774cbf21aa704980754be047679ca7d81cd2ac06
PatrickDougan <63160155+PatrickDougan@users.noreply.github.com>
2021-05-29 16:23:04 -0800

    XSS and component vulnerabilities
    All files for XSS and vulnerable component

---
+++ XSS-Component/main.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const handlebars = require('express-handlebars')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.engine('hbs', handlebars({
+        extname: 'hbs',
+        defaultLayout: 'index'
+}));
+app.set('view engine', 'hbs')
+
+const con = mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'password',
+        database: 'commentdb'
+
+});
+
+con.connect(function(Error){
+        if(Error){
+                throw Error;
+        }
+});
+
+
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false})
+
+app.get('/com', function(req, res) {
+	res.render('aug', {title: 'download page', layout: 'aug'});
+});
+
+app.get('/', function(req, res)  {
+        var sql = "SELECT * FROM comments"
+        con.query(sql, function(err, result) {
+                if (err) throw err;
+		res.setHeader('Content-Type', 'text/html')
+		res.render('index', {results: result})
+        });
+        //var context = {kids: 'hello world'}
+        //res.render('index', context)
+        //res.sendFile('/home/cody/Documents/index.html');
+});
+
+app.post('/', urlencodedParser,  function(req, res) {
+        var body = req.body;
+        var sql = "INSERT INTO comments (name, email, comment) VALUES ('"+body.name+"','"+body.email+"','"+body.comment+"')";
+        con.query(sql, function (err, result) {
+                if (err) throw err;
+                res.redirect('/');
+                //console.log("Number of records insertd: " + result.affectedRows);
+        });
+});
+
+app.listen(port, () => {
+        console.log("sample running");
+});
